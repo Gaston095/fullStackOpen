d0c0a034e113aaa3bc9a3a832e11403f523c8179
Gaston095 <Gastonsalas095@gmail.com>
2024-07-29 10:23:57 -0300

    Paso 1, recopilar comentarios

---
+++ Part 1/unicafe/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+const App = () => {
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <>
+      <h1>Give feedback</h1>
+      <button onClick={()=>{setGood(good +1)}}>good</button>
+      <button onClick={()=>{setNeutral(neutral +1)}}>neutral</button>
+      <button onClick={()=>{setBad(bad +1)}}>bad</button>
+
+      <h1>Statics</h1>
+      <p>{`good ${good}`}</p>
+      <p>{`neutral ${neutral}`}</p>
+      <p>{`bad ${bad}`}</p>
+    </>
+  )
+}
+
+export default App
