bad61e5cab00315d2015928d94215321128dc59e
Gaston095 <Gastonsalas095@gmail.com>
2024-07-30 09:53:17 -0300

    Paso 4, mostrar estadísticas solo una vez que se hayan recopilado los comentarios

--- Part 1/unicafe/src/Statistics.jsx
+++ Part 1/unicafe/src/Statistics.jsx
@@ -1,16 +1,22 @@
-export default function Statistics({good, neutral, bad}) {
+export default function Statistics({ good, neutral, bad }) {
   const all = good + neutral + bad;
   const average = all === 0 ? 0 : (good - bad) / all;
   const positive = all === 0 ? 0 : (good * 100) / all;
   return (
     <>
-      <h1>Statics</h1>
-      <p>{`good ${good}`}</p>
-      <p>{`neutral ${neutral}`}</p>
-      <p>{`bad ${bad}`}</p>
-      <p>{`all ${all}`}</p>
-      <p>{`average ${average}`}</p>
-      <p>{`positive ${positive} %`}</p>
+      {all === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <div>
+          <h1>Statics</h1>
+          <p>{`good ${good}`}</p>
+          <p>{`neutral ${neutral}`}</p>
+          <p>{`bad ${bad}`}</p>
+          <p>{`all ${all}`}</p>
+          <p>{`average ${average}`}</p>
+          <p>{`positive ${positive} %`}</p>
+        </div>
+      )}
     </>
   );
 }
